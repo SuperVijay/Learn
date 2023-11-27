f2f719cb4d16e4ea87bf5b30d9a27eb9d0a552c0
Vijay Canakala <vjcany@yahoo.com>
2023-11-26 23:10:51 -0500

    reinvitentagenda

---
+++ awsReinvent/agenda.sql
@@ -0,0 +1,83 @@
+Workshop
+
+Mandalay Bay | Level 2 | South | Mandalay Bay Ballroom D
+
+Nov. 27 | 8:30 AM - 10:30 AM (PST)
+CMP401 | Build a cost-efficient Apache Spark data pipeline on Amazon EKS
+
+----------------
+
+DAT306-R | Improve resilience of database workloads by using chaos engineering [REPEAT]
+
+Builders Session
+
+Mandalay Bay | Level 2 | South | Surf D
+
+Nov. 27 | 10:00 AM - 11:00 AM (PST)
+
+----------------
+SVS307 | Scaling serverless data processing with Amazon Kinesis and Apache Kafka
+
+Breakout Session
+
+Mandalay Bay | Level 2 | South | Oceanside D
+
+Nov. 27 | 10:00 AM - 11:00 AM (PST)
+------------------
+COM203 | Unlocking your full potential with the power of generative AI on AWS
+
+Breakout Session
+
+Mandalay Bay | Level 2 | South | Reef F
+
+Nov. 27 | 10:00 AM - 11:00 AM (PST)
+----------------------
+SVS303-R | Building secure serverless applications workshop [REPEAT]
+
+Workshop
+
+Mandalay Bay | Level 1 | North | Islander C
+
+Nov. 27 | 11:30 AM - 1:30 PM (PST)
+--------------------------
+DAT338 | Data patterns for generative AI applications
+
+Breakout Session
+
+Mandalay Bay | Level 3 | South | Jasmine F
+
+Nov. 27 | 1:30 PM - 2:30 PM (PST)
+
+------------------------------
+AIM218 | Build your first generative AI application with Amazon Bedrock
+
+Breakout Session
+
+Mandalay Bay | Level 2 | South | Oceanside B
+
+Nov. 27 | 2:30 PM - 3:30 PM (PST)
+--------------------------------
+COM308-R1-SC2 | Serverless data streaming: Amazon Kinesis Data Streams and AWS Lambda [REPEAT] [SIMULCAST]
+
+Breakout Session
+
+Mandalay Bay | Level 2 | South | Mandalay Bay Ballroom L, Content Hub, Teal Screen
+
+Nov. 27 | 3:00 PM - 4:00 PM (PST)
+
+-----------------------------------
+AIM219-R1 | Learn and experiment with LLMs in Amazon SageMaker Studio Lab [REPEAT]
+
+Builders Session
+
+Mandalay Bay | Level 2 | South | Surf D
+
+Nov. 27 | 4:00 PM - 5:00 PM (PST)
+----------------------------------
+AIM355-R | Build conversational experiences with Amazon Lex and generative AI [REPEAT]
+
+Workshop
+
+Mandalay Bay | Level 2 | South | Oceanside A
+
+Nov. 27 | 5:00 PM - 7:00 PM (PST)
